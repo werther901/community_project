da167e1c13cd8af777d62b3fc0a791a40f25d2ad
ihoek <ihoek679@naver.com>
2025-03-10 11:48:19 +0900

    feat : 상세 페이지 db연결

--- Controller/CategoryContoller.js
+++ Controller/CategoryContoller.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcrypt"); // 비밀번호 암호화 라이브러리
 
 require("dotenv").config(); // env
 
-const { Category } = require("../models/index"); // 데이터베이스 모델
+const { Category, Write } = require("../models/index"); // 데이터베이스 모델
 
 // 카테고리 요청 - all
 const getCategory = async (req, res) => {
@@ -30,7 +30,14 @@ const getCategoryOne = async (req, res) => {
   res.send({ name: categoryname.dataValues.name });
 };
 
+//전체 포스트 요청 - all
+const allPost = async (req, res) => {
+  let allpost = await Write.findAll({}).catch((err) => console.log(err));
+  res.send({ allpost });
+};
+
 module.exports = {
   getCategory,
   getCategoryOne,
+  allPost,
 };
--- Routes/DetailmainRouter.js
+++ Routes/DetailmainRouter.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const CategoryController = require("../Controller/CategoryContoller");
 
 router.post("/categoryselcet", CategoryController.getCategoryOne);
+router.post("/allPost", CategoryController.allPost); //전체 포스트 검색
 
 module.exports = router;
